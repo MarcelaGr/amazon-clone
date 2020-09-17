f4f54a1e65a9182d769f6aec5d7428b464a3db26
Marcela Grecco <marcee.grecco@gmail.com>
2020-09-16 21:32:32 -0300

    version 1

---
+++ amazone-clone/src/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="https://shityoucanafford.com/wp-content/uploads/2018/11/font-page-banner-template-v2.png" alt="Prime Video" />
+            </div>
+
+            <div className="home__row">
+                {/* Product */}
+                {/* Product */}
+            </div>
+
+            <div className="home__row">
+                {/* Product */}
+                {/* Product */}
+                {/* Product */}
+            </div>
+
+            <div className="home__row">
+                {/* Product */}
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ amazone-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCWCNsusiZoAlb_RI6WkmDh14GRmX1ZvYY",
+  authDomain: "mar-3e510.firebaseapp.com",
+  databaseURL: "https://mar-3e510.firebaseio.com",
+  projectId: "mar-3e510",
+  storageBucket: "mar-3e510.appspot.com",
+  messagingSenderId: "91389367152",
+  appId: "1:91389367152:web:5052fde2074c3265cc75b5"
+};
